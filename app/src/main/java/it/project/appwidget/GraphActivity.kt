9d92edca8f29e0480d26a5eb9c2b602b93c3dba9
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-05-13 19:45:18 +0200

    Aggiunta funzionalità al bottone di generare vettore di dati da mostrare sul grafico

--- app/src/main/java/it/project/appwidget/GraphActivity.kt
+++ app/src/main/java/it/project/appwidget/GraphActivity.kt
@@ -2,10 +2,24 @@ package it.project.appwidget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlin.random.Random
 
 class GraphActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graph)
+
+        val generateButton: Button = findViewById(R.id.generateButton)
+        val barChart: BarChart = findViewById(R.id.barChart)
+        generateButton.setOnClickListener{generateButton: View ->
+            generateButton as Button
+            // Creo nuovo vettore di interi
+            val values = IntArray(7) {
+                Random.nextInt(100)
+            }
+            barChart.valueArray = values
+        }
     }
 }
