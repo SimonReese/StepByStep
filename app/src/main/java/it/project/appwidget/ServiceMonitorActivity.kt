abd9651b30174e4e6ef26982c0492a16624c75d3
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-05-14 17:22:10 +0200

    Creata nuova activity per la gestione di LocationService.
    Aggiunta activity a manifest, creato layout e creata classe corrispondente.

---
+++ app/src/main/java/it/project/appwidget/ServiceMonitorActivity.kt
@@ -0,0 +1,11 @@
+package it.project.appwidget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServiceMonitorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service_monitor)
+    }
+}
