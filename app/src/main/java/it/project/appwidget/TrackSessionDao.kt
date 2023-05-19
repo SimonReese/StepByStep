aa310a2d69dda2c8af8491e6d0156631014532b1
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-05-20 01:28:08 +0200

    Documentazione queries.

--- app/src/main/java/it/project/appwidget/TrackSessionDao.kt
+++ app/src/main/java/it/project/appwidget/TrackSessionDao.kt
@@ -8,24 +8,47 @@ import androidx.room.Query
 
 @Dao
 interface TrackSessionDao {
+
+    /**
+     * Inserisce nuovo oggetto TrackSession nel database
+     */
     @Insert
     fun insertSession(trackSession: TrackSession)
 
+    /**
+     * Rimuove oggetto TrackSession dal database
+     */
     @Delete
     fun delete(trackSession: TrackSession)
 
+    /**
+     * Aggiorna oggetto TrackSession nel database
+     */
     @Query("UPDATE track_sessions SET activityType = :activityType WHERE id = :sessionId")
     fun updateActivityType(sessionId: Int, activityType: String)
 
+    /**
+     * Restituisce tutti gli oggetti TrackSession nel database
+     */
     @Query("SELECT * FROM track_sessions")
     fun getAllTrackSessions(): LiveData<List<TrackSession>>
 
+    /**
+     * Restituisce tutti gli oggetti TrackSession nel database tramite id
+     */
+    // TODO: refactor del nome - se cerco per id dovrò avere 1 solo risultato - getTracksession by id
     @Query("SELECT * FROM track_sessions WHERE id = :sessionId")
     fun getAllTrackSessionsById(sessionId: Int): List<TrackSession>
 
+    /**
+     * Restituisce tutti gli oggetti TrackSession nel database tra due date
+     */
     @Query("SELECT * FROM track_sessions WHERE startTime >= :startTime AND startTime <= :endTime")
     fun getTrackSessionsBetweenDates(startTime: Long, endTime: Long): List<TrackSession>
 
+    /**
+     * ? //TODO: è utile?
+     */
     @Query("SELECT id,startTime FROM track_sessions")
     fun getSessionIdsAndStartTimes(): List<SessionIdStartTime>
 
