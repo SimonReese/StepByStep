cdcbc2acf59b8757911d90683efdc6b73bdbe213
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-05-21 22:09:10 +0200

    Inserita parziale documentazione attributi entità.

--- app/src/main/java/it/project/appwidget/TrackSession.kt
+++ app/src/main/java/it/project/appwidget/TrackSession.kt
@@ -11,11 +11,40 @@ import java.util.Date
 @Entity(tableName = "track_sessions")
 data class TrackSession(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
+
+    /**
+     * Ora di inizio attività registrata. Corrisponde alla Unix epoch time della prima location registrata.
+     */
     val startTime: Long,
+
+    /**
+     * Ora di fine attività registrata. Corrisponde alla Unix epoch time dell'ultima location registrata.
+     */
     var endTime: Long,
+
+    /**
+     * Durata attività registrata. Corrisponde alla differenza di tempo tra la prima e l'ultima location registrata.
+     */
     var duration: Long,
+
+    /**
+     * Distanza totale percorsa in metri. Non è la differenza delle distanze tra la prima e l'ultima
+     * location, ma piuttosto la somma delle distanze tra ogni location (in genere distanti circa 10m)
+     */
     var distance: Double,
+
+    /**
+     * TODO
+     */
     var averageSpeed: Double,
+
+    /**
+     * TODO
+     */
     var maxSpeed: Double,
+
+    /**
+     * TODO
+     */
     var activityType: String
 )
