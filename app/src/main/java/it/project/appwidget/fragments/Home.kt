ba672ef3f8c284b950168b3e025914254a72aa9a
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-05-24 22:33:13 +0200

    Pulizia codice.
    Rimossa variabile currentFragmentId poichè non serve: non è necessario sapere il fragment id alla distruzione e ri-creazione dell'activity poichè il fragment manager ha già impostato il fragment precedente.

--- app/src/main/java/it/project/appwidget/fragments/Home.kt
+++ app/src/main/java/it/project/appwidget/fragments/Home.kt
@@ -1,6 +1,7 @@
 package it.project.appwidget.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,60 @@ class Home : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("HomeFragment", "Chiamato onCreate")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("HomeFragment", "Chiamato onCreateView")
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("HomeFragment", "Chiamato onViewCreated")
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Log.d("HomeFragment", "Chiamato onViewStateRestored")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("HomeFragment", "Chiamato onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("HomeFragment", "Chiamato onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("HomeFragment", "Chiamato onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeFragment", "Chiamato onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d("HomeFragment", "Chiamato onSaveInstanceState")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("HomeFragment", "Chiamato onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HomeFragment", "Chiamato onDestroy")
     }
 }
