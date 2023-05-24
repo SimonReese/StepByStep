b6582875637927f503c5faf9ebeb819ddf0e9241
Filippo Galli <fgalli2001@gmail.com>
2023-05-24 15:19:37 +0200

    Nuova fun in TrackSessionDao

--- app/src/main/java/it/project/appwidget/database/TrackSessionDao.kt
+++ app/src/main/java/it/project/appwidget/database/TrackSessionDao.kt
@@ -21,6 +21,9 @@ interface TrackSessionDao {
     @Delete
     fun delete(trackSession: TrackSession)
 
+    @Query("DELETE FROM track_sessions")
+    fun deleteAll()
+
     /**
      * Aggiorna oggetto TrackSession nel database
      */
