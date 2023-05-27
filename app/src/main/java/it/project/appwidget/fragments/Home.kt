10ea4d2db573829d87aa2ee8c11146a900b14cab
Falafel001 <fgalli2001@gmail.com>
2023-05-27 17:40:15 +0200

    savedInstanceState Home

--- app/src/main/java/it/project/appwidget/fragments/Home.kt
+++ app/src/main/java/it/project/appwidget/fragments/Home.kt
@@ -1,18 +1,51 @@
 package it.project.appwidget.fragments
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.TextView
+import it.project.appwidget.LocationService
 import it.project.appwidget.R
+import java.text.DecimalFormat
+import java.util.concurrent.TimeUnit
 
 class Home : Fragment() {
+
+    // Views
+    private lateinit var distanceTextView: TextView
+    private lateinit var passiTextView: TextView
+
+
+    private lateinit var locationBroadcastReceiver: LocationBroadcastReceiver
+    // Classe per ricezione broadcast messages
+    private inner class LocationBroadcastReceiver(): BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d("Home.LocationBroadcastReceiver", "Chiamato onReceive")
+            val distloc = intent?.getFloatExtra("distance", 0f)
+
+            distanceTextView.text = (DecimalFormat("#.##").format(distloc)).toString()
+            passiTextView.text = (DecimalFormat("#.##").format(distloc!! *3/2)).toString()
+
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("HomeFragment", "Chiamato onCreate")
+
+        // Registro receiver
+        locationBroadcastReceiver = LocationBroadcastReceiver()
+        requireActivity().registerReceiver(locationBroadcastReceiver, IntentFilter("location-update"))
     }
 
     override fun onCreateView(
@@ -20,52 +53,57 @@ class Home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Log.d("HomeFragment", "Chiamato onCreateView")
-        // Inflate the layout for this fragment
+        // Inflate del layout
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //TODO: chiedere permessi
         Log.d("HomeFragment", "Chiamato onViewCreated")
-    }
 
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        Log.d("HomeFragment", "Chiamato onViewStateRestored")
-    }
+        // Inizializzazione Views
+        distanceTextView = view.findViewById<TextView>(R.id.counterDistance)
+        passiTextView = view.findViewById<TextView>(R.id.counterPassi)
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("HomeFragment", "Chiamato onStart")
-    }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("HomeFragment", "Chiamato onResume")
-    }
+        // Recupero stato del fragment, ma solo se onSaveInstanceState non è null
+        if (savedInstanceState != null) {
+            restoreState(savedInstanceState)
+        }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d("HomeFragment", "Chiamato onPause")
-    }
+        // Creo intent per il LocationService
+        val serviceIntent = Intent(requireActivity(), LocationService::class.java)
 
-    override fun onStop() {
-        super.onStop()
-        Log.d("HomeFragment", "Chiamato onStop")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
         Log.d("HomeFragment", "Chiamato onSaveInstanceState")
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.d("HomeFragment", "Chiamato onDestroyView")
+        // Salvo lo stato di tutte le Views
+        outState.putCharSequence("distanceTextView_text", distanceTextView.text)
+        outState.putCharSequence("passiTextView_text", passiTextView.text)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onDestroy() {
-        super.onDestroy()
         Log.d("HomeFragment", "Chiamato onDestroy")
+        // Tolgo registrazione receiver
+        requireActivity().unregisterReceiver(locationBroadcastReceiver)
+
+        super.onDestroy()
+    }
+
+    // Recupero stato del fragment
+    private fun restoreState(inState: Bundle) {
+        Log.d("HomeFragment", "Chiamato restoreState")
+
+
+        // Ripristino stato delle textviews
+        distanceTextView.text = inState.getCharSequence("distanceTextView_text")
+        passiTextView.text = inState.getCharSequence("passiTextView_text")
+
+
+
     }
 }
