a0a6cbe5aae50a4d5ec01a914f72143286f5b16d
giovannifaedo <giovannipuntofaedo73@gmail.com>
2023-06-01 20:44:27 +0200

    Impostato design e provato il funzionamento degli switch

--- app/src/main/java/it/project/appwidget/fragments/Config.kt
+++ app/src/main/java/it/project/appwidget/fragments/Config.kt
@@ -1,10 +1,15 @@
 package it.project.appwidget.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.Switch
+import androidx.navigation.fragment.NavHostFragment
 import it.project.appwidget.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -12,15 +17,12 @@ import it.project.appwidget.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Config.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Config : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var navigationHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +37,50 @@ class Config : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_config, container, false)
+        var rootView = inflater.inflate(R.layout.config_prova, container, false)
+
+        val switchBtn = rootView.findViewById<Switch>(R.id.switch_btn)
+
+        //prelevo il frame layout
+        val layout_container = rootView.findViewById<FrameLayout>(R.id.frame_layout)
+
+        /**
+         * switchBtn.setOnCheckedChangeListener { _, isChecked ->
+        Log.i("Switch", "dentro la funzione")
+        if(isChecked){
+        Log.i("Switch", "IsChecked = true")
+        //mostro layout 1
+        layout_container.removeAllViews()
+        val layout1 = inflater.inflate(R.layout.config_prova, layout_container, false)
+        layout_container.addView(layout1)
+        }else {
+        Log.i("Switch", "IsChecked = false")
+        //switch è disattivo
+        layout_container.removeAllViews()
+        val layout2 = inflater.inflate(R.layout.fragment_setup, layout_container, false)
+        layout_container.addView(layout2)
+        }
+        }
+         */
+
+        switchBtn.setOnClickListener {
+            if(switchBtn.isChecked){
+                Log.i("Switch", "IsChecked = true")
+                layout_container.removeAllViews()
+                val layout1 = inflater.inflate(R.layout.fragment_setup, layout_container, false)
+                layout_container.addView(layout1)
+            }else {
+                Log.i("Switch", "IsChecked = false")
+                //switch è disattivo
+                layout_container.removeAllViews()
+                val layout2 = inflater.inflate(R.layout.fragment_setup, layout_container, false)
+                layout_container.addView(layout2)
+            }
+        }
+
+
+        return rootView
+
     }
 
     companion object {
--- app/src/main/java/it/project/appwidget/fragments/Setup.kt
+++ app/src/main/java/it/project/appwidget/fragments/Setup.kt
@@ -1,10 +1,13 @@
 package it.project.appwidget.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.Switch
 import it.project.appwidget.R
 
 class Setup : Fragment() {
@@ -21,6 +24,28 @@ class Setup : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setup, container, false)
+        // Inflate the layout for this fragment
+        var rootView = inflater.inflate(R.layout.fragment_setup, container, false)
+
+        val switchBtn = rootView.findViewById<Switch>(R.id.switcher)
+
+        //prelevo il frame layout
+        val layout_container = rootView.findViewById<FrameLayout>(R.id.frame_layout)
+
+        switchBtn.setOnClickListener {
+            if (switchBtn.isChecked) {
+                Log.i("Switch Setup", "IsChecked = true")
+                layout_container.removeAllViews()
+                val layout1 = inflater.inflate(R.layout.config_prova, layout_container, false)
+                layout_container.addView(layout1)
+            }else{
+                Log.i("Switch Setup", "IsChecked = false")
+                layout_container.removeAllViews()
+                val layout2 = inflater.inflate(R.layout.config_prova, layout_container, false)
+                layout_container.addView(layout2)
+            }
+        }
+
+        return rootView
     }
 }
