12a96f7bc31912b107813e3eba9c758b172d0406
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-06-08 17:22:22 +0200

    Pulizia codice. Aggiunta documentazione

--- app/src/main/java/it/project/appwidget/widgets/GraphWidget.kt
+++ app/src/main/java/it/project/appwidget/widgets/GraphWidget.kt
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
@@ -25,10 +26,20 @@ import kotlinx.coroutines.launch
  */
 class GraphWidget : AppWidgetProvider() {
     companion object{
+
+        /** Costante per il prefisso del nome del file delle [SharedPreferences]. Associare questo nome all'id del widget*/
         const val SHARED_PREFERENCES_FILE_PREFIX = "it.project.appwidget.widgets.GraphWidget"
+        /** Costante per la chiave dell'impostazione relativa a questo tipo di widget*/
         const val DATA_SETTINGS = "data"
 
-        // Aggiornamento del singolo widget
+        /**
+         * Aggiorna in modo asincrono le views di un widget in base ai valore impostato sulle [SharedPreferences].
+         *
+         *
+         * - [context] : Necessario per accedere alle [SharedPreferences].
+         * - [appWidgetManager] : Istanza di [AppWidgetManager] che si occuperà di aggiornare effettivamente le views
+         * - [appWidgetId] : Id del widget da aggiornare
+         */
         fun updateWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
             Log.d("GrapWidget", "Avvio aggiornamento grafico")
 
@@ -76,11 +87,12 @@ class GraphWidget : AppWidgetProvider() {
 
     }
 
-
+    /** Il provider è stato abilitato poichè un widget è stato aggiunto allo schermo */
     override fun onEnabled(context: Context) {
         Log.d("GraphWidget", "Chiamato onEnabled")
     }
 
+    /** Il provider ha ricevuto un intent */
     override fun onReceive(context: Context, intent: Intent) {
         Log.d("GraphWidget", "Chiamato onReceive")
         super.onReceive(context, intent)
@@ -94,24 +106,24 @@ class GraphWidget : AppWidgetProvider() {
         }
     }
 
+    /** Il provider deve aggiornare i suoi widget */
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         Log.d("GraphWidget", "Chiamato onUpdate")
-        // There may be multiple widgets active, so update all of them
+
         for (appWidgetId in appWidgetIds) {
             updateWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
-    override fun onAppWidgetOptionsChanged(
-        context: Context?,
-        appWidgetManager: AppWidgetManager?,
-        appWidgetId: Int,
-        newOptions: Bundle?
-    ) {
+    /** Uno dei widget è stato ridimensionato */
+    override fun onAppWidgetOptionsChanged(context: Context?, appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int, newOptions: Bundle?) {
+
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
         Log.d("GraphWidget", "Chiamato onAppWidgetOptionsChanged")
     }
 
+    /** Uno o più widget sono stati eliminati*/
     override fun onDeleted(context: Context, appWidgetIds: IntArray) {
         Log.d("GraphWidget", "Chiamato onDeleted")
         // Rimuovo preferenze in modo asincrono
@@ -125,6 +137,7 @@ class GraphWidget : AppWidgetProvider() {
         }
     }
 
+    /** Il provider è stato disabilitato in quanto tutti i suoi widget non sono più presenti sullo schermo */
     override fun onDisabled(context: Context) {
         Log.d("GraphWidget", "Chiamato onDisabled")
     }
