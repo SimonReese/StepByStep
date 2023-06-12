553b8a4ace3ac92f0aa8cd164182ba944336cc3e
giovannifaedo <giovannipuntofaedo73@gmail.com>
2023-06-12 17:03:16 +0200

    Iniziato impostazione della visibilità delle TextViews in base alle dimensioni del widget

--- app/src/main/java/it/project/appwidget/widgets/ListWidget.kt
+++ app/src/main/java/it/project/appwidget/widgets/ListWidget.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.RemoteViews
 import it.project.appwidget.R
 import it.project.appwidget.ListWidgetService
@@ -53,10 +54,51 @@ class ListWidget : AppWidgetProvider() {
         Log.d("ListWidget", "Chiamato onReceive con intent:  $intent" )
     }
 
+    //quando il widget viene ridimensionato si entra in questo metodo
     override fun onAppWidgetOptionsChanged(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetId: Int, newOptions: Bundle?) {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+        Log.d("entro nel metodo ridimensionamento", "Ridimensione")
 
         // Quando viene aggiornata la dimensione, mostro/nascondo i TextViews a seconda
+
+        //recupero dimensioni da Bundle
+        val minWidth = newOptions?.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
+        val minHeight = newOptions?.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
+
+        val views = RemoteViews(context?.packageName, R.id.list_item_widget)
+
+
+        //stampo i valori di dimensione
+        Log.d("dimensione minWeight", "$minWidth")
+        Log.d("dimensione minHeight", "$minHeight")
+
+        if (minWidth != null && minHeight != null) {
+            if(minWidth <= 255 && minHeight < 188){
+                //mostro solo la scritta con la data
+                //TODO View.GONE non funziona, mostra lo stesso le views
+                Log.d("Widget piccolo - mostro solo data", "Show data")
+                views.setViewVisibility(R.id.item_textview, View.VISIBLE)
+                Log.d("Show text views", "Show tv")
+                views.setViewVisibility(R.id.item_distance, View.GONE)
+                Log.d("don't show distance", "don't show distance")
+                views.setViewVisibility(R.id.item_duration, View.GONE)
+                views.setViewVisibility(R.id.item_avg_speed, View.GONE)
+
+            }
+
+            if(minWidth > 255 && minHeight > 121){
+                //mostro tutto
+                Log.d("Widget larghezza grande - mostro tutto", "Show all")
+                views.setViewVisibility(R.id.item_textview, View.VISIBLE)
+                views.setViewVisibility(R.id.item_distance, View.VISIBLE)
+                views.setViewVisibility(R.id.item_duration, View.VISIBLE)
+                views.setViewVisibility(R.id.item_avg_speed, View.VISIBLE)
+
+            }
+
+
+
+        }
     }
 
 }
