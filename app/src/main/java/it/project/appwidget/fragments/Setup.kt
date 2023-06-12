0dfb9c51404ef2850c1b66f193e5bc287e526d3c
giovannifaedo <giovannipuntofaedo73@gmail.com>
2023-06-12 12:24:29 +0200

    Modificato la classe Setup per la gestione dello Spinner

--- app/src/main/java/it/project/appwidget/fragments/Setup.kt
+++ app/src/main/java/it/project/appwidget/fragments/Setup.kt
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.navigation.findNavController
 import com.google.android.material.textfield.TextInputLayout
@@ -16,7 +19,31 @@ import it.project.appwidget.UserPreferencesHelper
 
 class Setup : Fragment() {
     private lateinit var preferencesHelper: UserPreferencesHelper
+    private lateinit var genderSpinner: Spinner
+    private var positionSelected: Int = -1
 
+    /** elemento selezionato della lista*/
+    private lateinit var genderItem: String
+
+    /** Listener che si aggiorna quando seleziono un elemento diverso della lista*/
+    private val itemSelectedListener = ItemSelectedListener()
+
+
+    /** classe per la gestione dei click nello Spinner */
+    private inner class ItemSelectedListener: AdapterView.OnItemSelectedListener{
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            genderItem = parent?.getItemAtPosition(position) as String
+            positionSelected = position
+            Log.d("posizione Spinner", "$positionSelected")
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            //se non seleziono nulla prendo l'item di default (cioè M)
+            return
+        }
+
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         preferencesHelper = UserPreferencesHelper(requireContext())
@@ -27,7 +54,16 @@ class Setup : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_setup, container, false)
-
+        //Recupero lo spinner
+        val genderSpinner = view.findViewById<Spinner>(R.id.gender_Spinner)
+        // Imposto dati spinner
+        val spinnerAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.gender_configure_options, R.layout.gender_spinner_item)
+        spinnerAdapter.setDropDownViewResource(R.layout.gender_spinner_item)
+        // Inizializzo item selezionato
+        genderItem = spinnerAdapter.getItem(0) as String
+        // Imposto adapter e listener sullo spinner
+        genderSpinner.adapter = spinnerAdapter
+        genderSpinner.onItemSelectedListener = itemSelectedListener
         //Bottone salva
         val btnSave = view.findViewById<Button>(R.id.btn_save)
         btnSave.setOnClickListener {
@@ -36,7 +72,7 @@ class Setup : Fragment() {
                 val nomeUtente: String = (view.findViewById<TextInputLayout>(R.id.nome_utente)?.editText?.text ?: "").toString()
                 val peso: String = (view.findViewById<TextInputLayout>(R.id.peso)?.editText?.text ?: "").toString()
                 val eta: String = (view.findViewById<TextInputLayout>(R.id.eta)?.editText?.text ?: "").toString()
-                val sesso: String = (view.findViewById<TextInputLayout>(R.id.sesso)?.editText?.text ?: "").toString()
+                val sesso: String = genderItem
                 val kcalTarget: String = (view.findViewById<TextInputLayout>(R.id.kcalTarget)?.editText?.text ?: "").toString()
 
                 // Salva i dati utilizzando l'helper nelle preferenze condivise
@@ -45,6 +81,8 @@ class Setup : Fragment() {
                 preferencesHelper.eta = eta
                 preferencesHelper.sesso = sesso
                 preferencesHelper.kcalTarget = kcalTarget.toInt()
+
+                preferencesHelper.spinnerPosition = positionSelected
 
                 // Apri il fragment "config"
                 navigateToSetupFragment()
@@ -71,14 +109,14 @@ class Setup : Fragment() {
     }
 
     //Controlla che tutti i campi siano riempiti
+
     private fun areAllFieldsFilled(): Boolean {
         val nomeUtente: String = (view?.findViewById<TextInputLayout>(R.id.nome_utente)?.editText?.text ?: "").toString()
         val peso: String = (view?.findViewById<TextInputLayout>(R.id.peso)?.editText?.text ?: "").toString()
         val eta: String = (view?.findViewById<TextInputLayout>(R.id.eta)?.editText?.text ?: "").toString()
-        val sesso: String = (view?.findViewById<TextInputLayout>(R.id.sesso)?.editText?.text ?: "").toString()
         val kcalTarget: String = (view?.findViewById<TextInputLayout>(R.id.kcalTarget)?.editText?.text ?: "").toString()
 
-        return nomeUtente.isNotEmpty() && peso.isNotEmpty() && eta.isNotEmpty() && sesso.isNotEmpty() && kcalTarget.isNotEmpty()
+        return nomeUtente.isNotEmpty() && peso.isNotEmpty() && eta.isNotEmpty() && kcalTarget.isNotEmpty()
     }
 
     // Riempi TextInputLayout con i dati precedentemente inseriti
@@ -88,11 +126,13 @@ class Setup : Fragment() {
         val eta = preferencesHelper.eta
         val sesso = preferencesHelper.sesso
         val kcalTarget = preferencesHelper.kcalTarget
+        val positionSelected = preferencesHelper.spinnerPosition
+
 
         view?.findViewById<TextInputLayout>(R.id.nome_utente)?.editText?.setText(nomeUtente)
         view?.findViewById<TextInputLayout>(R.id.peso)?.editText?.setText(peso)
         view?.findViewById<TextInputLayout>(R.id.eta)?.editText?.setText(eta)
-        view?.findViewById<TextInputLayout>(R.id.sesso)?.editText?.setText(sesso)
+        view?.findViewById<Spinner>(R.id.gender_Spinner)?.setSelection(positionSelected)
         view?.findViewById<TextInputLayout>(R.id.kcalTarget)?.editText?.setText(kcalTarget.toString())
     }
 
