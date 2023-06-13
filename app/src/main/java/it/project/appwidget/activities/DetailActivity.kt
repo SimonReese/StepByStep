62735210146c344a68c98aa718c5c00c6d2d7559
SimonReese <59612841+SimonReese@users.noreply.github.com>
2023-06-14 01:09:16 +0200

    Aggiunta documentazione.

--- app/src/main/java/it/project/appwidget/activities/DetailActivity.kt
+++ app/src/main/java/it/project/appwidget/activities/DetailActivity.kt
@@ -7,33 +7,47 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import it.project.appwidget.R
 import it.project.appwidget.database.AppDatabase
+import it.project.appwidget.database.TrackSession
+import it.project.appwidget.fragments.Stats
 import it.project.appwidget.util.WeekHelper
+import it.project.appwidget.widgets.ListWidget
 import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 
 
-//Activity contenente i dettagli della sessione selezionata
+/**
+ * Questa activity mostra i dettagli della [TrackSession] selezionata, sia dal fragment [Stats] che dal
+ * widget [ListWidget].
+ */
 class DetailActivity : AppCompatActivity() {
 
-    // Tag for Log messages
+    /** Tag per messaggi di [Log] */
     private val mTAG = this::class.simpleName
 
     companion object {
+        /** Chiave dell'Id della [TrackSession] da recuperare dal [Bundle] */
         const val ARG_SESSION_ID = "session:id"
     }
 
+    /** [TextView] relativa alla data di inizio della [TrackSession] */
     private lateinit var tv_startData: TextView
+    /** [TextView] relativa alla data di fine della [TrackSession] */
     private lateinit var tv_endData: TextView
+    /** [TextView] relativa alla tipologia di [TrackSession] */
     private lateinit var tv_typeData: TextView
+    /** [TextView] relativa alla distanza della [TrackSession] */
     private lateinit var tv_distanceData: TextView
+    /** [TextView] relativa alla durata della [TrackSession] */
     private lateinit var tv_timeData: TextView
+    /** [TextView] relativa alla velocità media della [TrackSession] */
     private lateinit var tv_avrSpeedData: TextView
+    /** [TextView] relativa alle calorie della [TrackSession] */
     private lateinit var tv_calorieData: TextView
 
     // TODO: activity sparisce alla rotazione - ritorno al fragment precedente
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d(mTAG, "Chiamato onCreate()")
         setContentView(R.layout.activity_session_detail)
 
         // Riferimenti alle Views
@@ -49,7 +63,7 @@ class DetailActivity : AppCompatActivity() {
         // Ottengo Id della sessione cliccata
         val sessionId = intent.getIntExtra(ARG_SESSION_ID, -1)
 
-        println("Id ricevuto in DetailActivity: " + sessionId)
+        Log.d(mTAG, "Ricevuto id $sessionId")
 
         if (sessionId == -1){
             return
@@ -74,6 +88,7 @@ class DetailActivity : AppCompatActivity() {
         Log.v(mTAG, "onDestroy() called")
     }
 
+    /** Carica i dati dal database [AppDatabase] in modo asincrono, cercando la [TrackSession] per id */
     private fun loadData(sessionId: Int){
         lifecycleScope.launch {
             // Dall'Id ottengo tutte le informazioni sulla sessione
@@ -87,14 +102,14 @@ class DetailActivity : AppCompatActivity() {
             val minutes = (duration % 3600) / 60
             val seconds = duration % 60
 
-            //imposto il testo per tutte le views in base ai dati che ho ricevuto dall'intent
+            // Imposto il testo per tutte le views in base ai dati che ho ricevuto dall'intent
             tv_startData.text = WeekHelper.getDate(trackSession.startTime, format)
             tv_endData.text = WeekHelper.getDate(trackSession.endTime, format)
             tv_typeData.text = trackSession.activityType
-            //aggiunta modifica per la distanza in km
-            tv_distanceData.text = noDecimal.format(trackSession.distance/1000) + "km"
+            // Aggiunta modifica per la distanza in km
+            tv_distanceData.text = noDecimal.format(trackSession.distance / 1000) + "km"
             tv_timeData.text = "" + hours + "h " + minutes + "min " + seconds + "sec"
-            //aggiunta modifica per la velocità media in km/h
+            // Aggiunta modifica per la velocità media in km/h
             tv_avrSpeedData.text = DecimalFormat("#.#").format(trackSession.averageSpeed * 3.6) + "km/h"
             tv_calorieData.text = noDecimal.format(trackSession.kcal) + "Kcal"
 
